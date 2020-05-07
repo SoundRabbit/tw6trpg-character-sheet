e88bd9d79c682fe96375c4e2e60f339db206c941
SoundRabbit <soundrabbitstd@gmail.com>
2020-05-07 20:57:57 +0900

    Define State

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,35 @@ import { Form, Button } from "react-bootstrap"
 
 type Props = {}
 
-type State = {}
+type State = {
+    character: Character;
+}
+
+type Character = {
+    name: string;
+    race: string;
+    job: string;
+    damage: number;
+    exp: number;
+    description: string;
+    ubelCodes: UbelCode[];
+    items: Item[];
+    skills: Skills;
+}
+
+type UbelCode = {
+    name: string,
+    aria: string,
+    customTexts: string[],
+    description: string,
+}
+
+type Item = {
+    name: string,
+    level: number,
+}
+
+type Skills = Map<string, number>
 
 export class App extends React.Component<Props, State> {
     constructor(props: Props) {
